dbd12bc7bdf9fcdf185a6944bb9a2e512fa258de
Angel Vasquez <AngelVasquezBruno@gmail.com>
2023-01-02 23:43:38 -0500

    Fully responsive google search engine using Google api and next.js serverside rendering to ensure an optimal user friendly experience

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API KEY - https://developer.google.com/custom-search/v1/using_rest
+export const API_KEY =`AIzaSyBNstHxATkt74o5ge7XcGp_CuHVm9jIGV0`
+
+// Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = `6246f2fdef78347c7`
